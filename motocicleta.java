508849cee500f9f174e918f341f40a9a8b195434
carlosmateus <55406490+carlosmateus@users.noreply.github.com>
2026-03-30 21:33:17 -0300

    Create motocicleta.java

---
+++ motocicleta.java
@@ -0,0 +1,14 @@
+public class Motocicleta extends Veiculo
+{
+    private int rodas;
+    
+    public int getRodas()
+    {
+        return(rodas);
+    }
+    
+    public void setRodas(int rodas)
+    {
+        this.rodas=rodas;
+    }
+}
