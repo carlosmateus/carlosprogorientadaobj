6101eb2317161772a470ea76818cb054d15c7156
carlosmateus <55406490+carlosmateus@users.noreply.github.com>
2026-03-30 21:32:42 -0300

    Add Automovel class extending Veiculo

---
+++ Automovel.java
@@ -0,0 +1,14 @@
+public class Automovel extends Veiculo
+{
+    private int porta;
+    
+    public int getPorta()
+    {
+        return(porta);
+    }
+    
+    public void setPorta(int porta)
+    {
+        this.porta=porta;
+    }
+}
